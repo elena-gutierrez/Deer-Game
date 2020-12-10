5eda765643b27a2fdd99dae8399e74c4f792c7e7
Veronica7112 <flamboyantflowers123@gmail.com>
2020-12-10 09:12:47 -0800

    Update 1.js

--- Puzzle design/1.js
+++ Puzzle design/1.js
@@ -1,9 +1,69 @@
+
+  //create an empty array called balls
+
+//create a variable to hold your avatar
+let me;
+
+
 function setup() {
-  createCanvas(600, 600);
-  rectangle(50,0,150,400)
-  rectangle(250,0,350,400)
-  rectangle(400,0,500,400)
+  createCanvas(500, 400);
+
+  //make one avatar called me
+  me = new Avatar(width/2, 300, 3);
+
 }
 
-function draw() {
-  background("white");
+function draw(){
+
+}
+
+//avatar class
+class Avatar {
+
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+        this.speed = speed;
+	}
+
+	drawMe(){  // draw the running person
+    		stroke("green");
+        strokeWeight(3);
+    		fill("blue");
+		    ellipse(this.x,this.y,20,20);
+        line(this.x,this.y, this.x, this.y+40);
+        line(this.x, this.y+40, this.x-20, this.y+60);
+        line(this.x, this.y+40, this.x+10, this.y+50);
+        line(this.x+10, this.y+50, this.x+5, this.y+60);
+        line(this.x, this.y+15, this.x-10, this.y+25);
+        line(this.x-10, this.y+25, this.x+10, this.y+35);
+	}
+
+	moveMe(){
+    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
+       this.y -= this.speed;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
+        this.y += this.speed;
+    }
+	}
+
+  die(){
+
+  }
+
+}
+
+
+//ball class from which to create new balls with similar properties.
+class Ball {
+
+	//every ball needs an x value, a y value, and a speed
+	constructor(x,y, speed){
+		this.x = x;
+    this.y = y;
+    this.speed = speed;
+	}
+
+	// draw a ball on the screen at x,y
